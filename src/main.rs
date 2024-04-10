5e4909d8d086d9e1bf0b269988075de6368fc042
wtlin1228 <wtlin1228@gmail.com>
2024-04-11 03:19:44 +0800

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 use std::os::unix::fs::FileExt;
 
 use sqlite_starter_rust::database_file_header::DatabaseFileHeader;
-use sqlite_starter_rust::BTreePage;
+use sqlite_starter_rust::{BTreePage, SchemaTable};
 
 fn main() -> Result<()> {
     // Parse arguments
@@ -31,7 +31,7 @@ fn main() -> Result<()> {
             let page = BTreePage::new(&buff, Some(database_file_header))?;
             println!("number of tables: {}", page.cell_count);
         }
-        ".table" => {
+        ".tables" => {
             let mut file = File::open(&args[1])?;
             let mut buff = [0; 100];
             file.read_exact(&mut buff)?;
@@ -42,6 +42,21 @@ fn main() -> Result<()> {
             let mut buff = vec![0u8; page_size as usize];
             file.read_exact_at(&mut buff, 0)?;
             let page = BTreePage::new(&buff, Some(database_file_header))?;
+
+            let mut tbl_names = vec![];
+            for i in 0..page.cell_pointers.len() {
+                let cell = match i {
+                    0 => &buff[page.cell_pointers[0] as usize..],
+                    _ => &buff[page.cell_pointers[i] as usize..page.cell_pointers[i - 1] as usize],
+                };
+                let schema_table = SchemaTable::from(cell)?;
+
+                // ref: https://www.sqlite.org/fileformat2.html#internal_schema_objects
+                if !schema_table.tbl_name.starts_with("sqlite_") {
+                    tbl_names.push(schema_table.tbl_name);
+                }
+            }
+            println!("{:?}", tbl_names.join(" "));
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
